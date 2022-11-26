8197794bd76c74e6ee6727842c6974dd38b7f1fb
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-11-26 20:05:18 -0300

    Add files via upload

---
+++ introdução a linguagem python/dir_e_help.py
@@ -0,0 +1,14 @@
+"""
+Utilitários Python para que auxiliar na prgramação
+
+Dir -> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável
+
+Dir(tipo de dado/variável)
+
+Help -> Apresenta a documentação/ como utilizar os atributos/propriedas de funções/métodos disponíveos para
+determinado tipo de dado ou variável.
+
+help(tipo de dado/veriavel.propriedade)
+"""
+print(dir('geek'))
+
