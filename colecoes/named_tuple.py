725625342baf4282ef7e73c8c07a8a595c796482
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-12-06 13:51:37 -0300

    Add files via upload

---
+++ colecoes/named_tuple.py
@@ -0,0 +1,60 @@
+"""
+Módulo Collections - Named Tuple
+"""
+
+# Recap tupla
+tupla = (1, 2, 3)
+
+print(tupla[1])
+
+"""
+Named Tuple -> São tupla, diferenciadas, onde,  especificamos um nome para a mesma e também parâmetros.
+"""
+
+# Importando
+
+from collections import namedtuple
+
+# Precisamos definir o nome e parâmetros.
+
+# Forma 1 - Declaração Named Tuple
+
+cachorro = namedtuple('cachorro', 'idade raca nome')
+
+# Forma 2 - Declaração Named Tuple
+
+cachorro = namedtuple('cachorro', 'idade, raca, name')
+
+#  Forma 3 - Declaração Named Tuple
+
+cachorro = namedtuple('cachoro', ['idade', 'raca', 'nome'])
+
+# Usando
+
+ray = cachorro(idade=2, raca='Chow-Chow', nome='Ray')
+
+print(ray)
+
+# Acessando os dados
+
+# Forma 1
+
+print(ray[0])  # idade
+print(ray[1])  # raça
+print(ray[2])  # nome
+
+# Forma 2
+
+"""
+Forma diferenciada de fazer acesso
+"""
+
+print(ray.idade)  # idade
+print(ray.raca)  # raca
+print(ray.nome)  # nome
+
+print(ray.index('Chow-Chow'))
+
+print(ray.count('Chow-Chow'))
+
+
