c84170d90e3d01428479a7736e616a2a6b9bd67a
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-12-18 16:53:14 -0300

    Add files via upload

---
+++ lambdas e funções integradas/min-e-max.py
@@ -0,0 +1,107 @@
+"""
+Min e Max
+
+max() -> Retorna o maior alor em um iterável ou o maior de dois ou mais elementos.
+"""
+
+# Exemplos
+
+lista = [1, 8, 4, 99, 34, 129]
+print(max(lista))  # 129
+
+tupla = (1, 8, 4, 99, 34, 129)
+print(max(tupla))
+
+conjunto = {1, 8, 4, 99, 34, 129}
+print(max(conjunto))  # 129
+
+dicionario = {'a': 1, 'b': 2, 'c': 4, 'd': 23, 'f': 21}
+print(max(dicionario.values()))
+
+print(max(3, 34))
+
+# Faça um programa que receba dois valores do usuário e mostre o maior
+# val1 = int(input('Informe o primeiro valor: '))
+# val2 = int(input('Informe o segundo valor: '))
+
+# print(max(val1, val2))
+
+print(max(4, 67, 54))
+
+print(max('a', 'ab', 'abc'))
+
+print(max(4, 67, 54))
+
+print(max(3.123, 123123))
+
+"""
+min() -> Retorna o menor valor em um iterável ou o menor de dois ou mais elementos
+
+"""
+
+# Exemplos
+
+lista = [1, 8, 4, 99, 34, 129]
+print(min(lista))  # 1
+
+tupla = (1, 8, 4, 99, 34, 129)
+print(min(tupla)) # 1
+
+conjunto = {1, 8, 4, 99, 34, 129}
+print(min(conjunto))  # 1
+
+dicionario = {'a': 1, 'b': 2, 'c': 4, 'd': 23, 'f': 21}
+print(min(dicionario.values()))
+
+print(min(3, 34))
+
+# Outros exemplos
+
+nomes = ['Arya', 'Samson', 'Dora', 'Tim', 'Ollivander']
+
+print(max(nomes))  # Tim
+
+print(min(nomes))  # Arya
+
+print(max(nomes, key=lambda nome: len(nome)))  # Ollivander
+
+print(max(nomes, key=lambda nome: len(nome)))  # Tim
+
+####
+
+musicas = [
+    {'titulo': 'Thunderstruck', 'tocou': 3},
+    {'titulo': 'Dead Skin Mask', 'tocou': 2},
+    {'titulo': 'Back in Black', 'tocou': 4},
+    {'titulo': 'Too old to rock in roll,  to ...', 'tocou': 32},
+]
+
+# key recebe uma função
+print(max(musicas, key=lambda musica: musica['tocou']))
+print(min(musicas, key=lambda musica: musica['tocou']))
+
+# DESAFIO! Imprima somente o título da música mais e menos tocada
+print(max(musicas, key=lambda musica: musica['tocou'])['titulo'])
+print(min(musicas, key=lambda musica: musica['tocou'])['titulo'])
+
+# DESAFIO! Cmo encontrar a  música mais tocada e a menos tocada sem usar max(), min() e lambda
+
+max = 0
+for musica in musicas:
+    if musica['tocou'] > max:
+        max = musica['tocou']
+
+for musica in musicas:
+    if musica['tocou'] == max:
+        print(musica['titulo'])
+
+
+min = 99999
+for musica in musicas:
+    if musica['tocou'] < min:
+        min = musica['tocou']
+
+for musica in musicas:
+    if musica['tocou'] == min:
+        print(musica['titulo'])
+
