75f9c3641965897b1dd14b0bb4ac1600ae4bcffa
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-12-22 07:32:18 -0300

    Add files via upload

---
+++ trabalhando com módulos/modulos_customizados.py
@@ -0,0 +1,20 @@
+"""
+Módulos Customizados
+
+Como módulos Python nada mais são do que arquivos Python, então TODOS os arquivos que criarmos
+neste curso são módulos Python pronto para serem utilizados.
+"""
+
+"""
+# Inportando uma funções específica do nosso módulo
+from funcoes_com_parametro_padrao import soma
+
+print(soma([1, 2]))
+"""
+
+"""
+# Importando todo o módulo (Temos acesso a TODOS os elementos do módulo)
+import funcoes_com_parametro_padrao
+"""
+
+
