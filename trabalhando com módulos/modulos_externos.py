9456969b4820216f664cc51019dd08e2b1f1c670
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-12-22 09:17:47 -0300

    Add files via upload

---
+++ trabalhando com módulos/modulos_externos.py
@@ -0,0 +1,15 @@
+"""
+Módulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+
+Você pode cocnhecer todos os pacotes externos oficiais em: https://pypi.org
+
+colorama -> É utilzado para permitir impressão de textos coloridos
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+
+"""
+
+
