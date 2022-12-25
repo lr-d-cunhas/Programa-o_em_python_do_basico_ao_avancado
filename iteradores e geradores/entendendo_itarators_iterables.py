d11a873a0beade63e70ec99421eac314121cb15f
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2022-12-25 09:57:26 -0300

    Add files via upload

---
+++ iteradores e geradores/entendendo_itarators_iterables.py
@@ -0,0 +1,36 @@
+"""
+Entendendo Iteratos e Iterables
+
+Iterator ->
+    - Objeto que pode ser iterado.
+    - Objeto que reetorna um dado, sendo um elemento por vez quando uma função next() é chamada;
+
+Iterable ->
+    - Um objeto que irá retornar um iterator quando a função iter() for chamada.
+
+
+"""
+
+nome = 'Geek'  # É um iterable mas não é um iterator.
+numeros = [1, 2, 3, 4, 5, 6]  # É um iterable mas não é um iterator.
+
+it1 = iter(nome)
+it2 = iter(numeros)
+
+print(next(it1))
+print(next(it1))
+print(next(it1))
+print(next(it1))
+print()
+print(next(it2))
+print(next(it2))
+print(next(it2))
+print(next(it2))
+print(next(it2))
+print(next(it2))
+
+nome = 'Geek'
+
+for letra in nome:
+    print(f'{letra}')
+
