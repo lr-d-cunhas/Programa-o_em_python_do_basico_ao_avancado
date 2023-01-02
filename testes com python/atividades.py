7eb9d82b8c316b640107b6c058a4605416257574
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-01-02 10:18:13 -0300

    Add files via upload

---
+++ testes com python/atividades.py
@@ -0,0 +1,14 @@
+def comer(comida, eh_saudavel):
+    if eh_saudavel:
+        final = 'quero manter a forma.'
+    else:
+        final = 'a gente só vive uma vez.'
+
+    return f'Estou comendo {comida} porque {final}'
+
+def dormir(num_horas):
+    if num_horas > 8:
+        return f'Ptz! Dormir muito! Estou atrasado para o trabalho!'
+    else:
+        return f'Continuo cansado após dormir por {num_horas} horas. :('
+
