36874fb69d8ffb9a4fec8b129563d977a13a9c7f
Rian Lucas <105382777+lr-d-cunhas@users.noreply.github.com>
2023-01-04 08:56:10 -0300

    Add files via upload

---
+++ novidades python 3.8/operador_warlus.py
@@ -0,0 +1,24 @@
+"""
+O operador Walrus permite fazer a atribuição e retorno de valor em uma única expressão.
+
+variavel := expressão
+"""
+
+nome = 'Geek University'
+print(nome)
+
+print(nome := 'Geek University')
+
+# Python 3.7
+cesta = []
+fruta = input('Informe a fruta: ')
+while fruta != 'jaca':
+    cesta.append(fruta)
+    fruta = input('informe a fruta: ')
+
+
+# Python 3.8
+cesta = []
+while (fruta := input('Informe a fruta: ')) != 'jaca':
+    cesta.append(fruta)
+
